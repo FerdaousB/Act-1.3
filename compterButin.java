58ff19bb08a919ce09e976de76877f417f8575fd
FerdaousB <91906989+FerdaousB@users.noreply.github.com>
2021-10-07 23:01:57 +0200

    Update compterButin.java

--- compterButin.java
+++ compterButin.java
@@ -1,13 +1,53 @@
 import java.util.Scanner;
+    public class compterButin
+    {
+     public static void main (String [] args)
+     {
+        int choix, add, mult, div, a, b;
+         div = 0;
+        do
+        {
+          Scanner scanner = new Scanner ( System . in ) ;
+          choix = scanner.nextInt( );
+          
 
-
-public class compterButin{
- 
-   
-  
-   public static void main(String[] args)
-   {
-       
-    
-   }
-}
+          
+          switch ( choix )
+          {
+            case 1 :
+             System.out.println ("Veuillez saisir un nombre: " );
+             a = scanner.nextInt( );
+             System.out.print ("Veuillez saisir un deuxieme nombre: \n");
+             b = scanner.nextInt( );
+             add = a + b;
+             System.out.println ("Le resultat est : "+ add);
+             break;
+             
+            case 2 :
+             System.out.println ("Veuillez saisir un nombre: \n");
+             a = scanner.nextInt( );
+             System.out.println ("Veuillez saisir un deuxieme nombre: \n");
+             b = scanner.nextInt( );
+             mult = a * b;
+             System.out.println ("Le resultat est : "+ mult);
+             break;
+             
+            case 3
+ :
+             System.out.println ("Veuillez saisir un nombre: \n");
+             a = scanner.nextInt( );
+             System.out.println ("Veuillez saisir un deuxieme nombre: \n");
+             b = scanner.nextInt( );
+             if ( b != 0 )
+             {
+               div = div + a / b;
+             }
+             
+             System.out.println ("Le resultat est : "+ div);
+             break;
+        
+            
+             
+           }
+         } while ( choix != 3);       }
+     }
